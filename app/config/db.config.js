b91dfaf56ca16259f4c86b124317f6be5bb104e1
raviteja8831 <75265374+raviteja8831@users.noreply.github.com>
2024-05-19 16:52:18 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "trackdot.c3k2cu0gij81.ap-southeast-1.rds.amazonaws.com:3306?ssl=false",
+  HOST: "trackdot.c3k2cu0gij81.ap-southeast-1.rds.amazonaws.com:3306",
   USER: "siddhu",
   PASSWORD: "GKbZNlpsgvJWtwGcFAUp",
   DB: "trackdot",
